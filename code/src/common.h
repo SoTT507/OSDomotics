7e873e12c3a08620e9be5360882848064daf62f8
r507 <riccardo.sottoriva@proton.me>
2026-05-19 08:53:06 +0200

    common.h definition

---
+++ code/src/common.h
@@ -0,0 +1,14 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+// --- Error codes as requested ---
+#define SUCCESS 0
+#define ERROR_DEVICE_NOT_FOUND -1
+#define ERROR_INVALID_COMMAND -2
+#define ERROR_LINK_FAILED -3
+#define ERROR_DEVICE_TYPE_MISMATCH -4
+
+// TODO: Add data structures for IPC messages as we progress
+//  (Inter-Process Communication)
+
+#endif // COMMON_H

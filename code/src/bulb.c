f25458804e71d6181d8e44924572bfa4db2073cf
r507 <riccardo.sottoriva@proton.me>
2026-06-22 02:17:53 +0200

    standardized response implemented in functions

--- code/src/bulb.c
+++ code/src/bulb.c
@@ -1,6 +1,7 @@
 #include "common.h"
 
 int my_id;
+int parent_id = 0;
 int is_on = 0; // 0 = off, 1 = on
 char my_fifo[128];
 int fifo_fd;
@@ -17,18 +18,34 @@ void cleanup_and_exit(int sig) {
 }
 
 // support function to send messages to the Controller
-void send_to_controller(const char* response_str) {
-    int ctrl_fd = open(CONTROLLER_FIFO, O_WRONLY);
-    if (ctrl_fd != -1) {
+void send_response(int requester_id, const char* response_str, int is_override) {
+  char target_fifo[128];
+  char final_message[MAX_CMD_LEN];  
+
+  if (is_override) {
+    snprintf(final_message, MAX_CMD_LEN, "OVERRIDE (Manual): %s", response_str);
+  } else {
+    strncpy(final_message, response_str, MAX_CMD_LEN);
+  }
+
+  if (requester_id == 0 || requester_id == -1) {
+    // respond to controller
+    strcpy(target_fifo, CONTROLLER_FIFO);
+  } else {
+    // respond to logical parent (Hub/Timer) that made the request
+    snprintf(target_fifo, sizeof(target_fifo), "%s%d.fifo", FIFO_PATH_PREFIX, requester_id);
+  }
+
+  int target_fd = open(target_fifo, O_WRONLY | O_NONBLOCK);
+    if (target_fd != -1) {
         IPC_Message response;
         response.sender_id = my_id;
-        response.target_id = 0; // 0 = Controller
+        response.target_id = (requester_id == -1) ? 0 : requester_id; // 0 = Controller
         strncpy(response.command, response_str, MAX_CMD_LEN);
-        
-        write(ctrl_fd, &response, sizeof(IPC_Message));
-        close(ctrl_fd);
+        write(target_fd, &response, sizeof(IPC_Message));
+        close(target_fd);
     } else {
-        perror("[Bulb] Errore nell'apertura della FIFO del Controller");
+        perror("[Bulb] Error trying to open target FIFO");
     }
 }
 
@@ -73,13 +90,17 @@ int main(int argc, char *argv[]) {
       // simulate processing latency (1 to 3 seconds)
       sleep((rand() % 3) + 1);
 
+
       int is_manual_override = (msg.sender_id == -1);
       char prefix[32];
+
+
       if (is_manual_override) {
         strcpy(prefix, "OVERRIDE (Manual)");
       } else {
         strcpy(prefix, "ACK");
       }
+
 
       if (strncmp(msg.command, "switch power on", 15) == 0) {
         if (!is_on) {
@@ -88,18 +109,23 @@ int main(int argc, char *argv[]) {
         }
         char response[MAX_CMD_LEN];
         snprintf(response, sizeof(response), "%s: Bulb %d turned ON", prefix, my_id);
-        send_to_controller(response);
+        send_response(msg.sender_id, response, is_manual_override);
+      } 
+      
 
-      } else if (strncmp(msg.command, "switch power off", 16) == 0) {
+      else if (strncmp(msg.command, "switch power off", 16) == 0) {
           if (is_on) {
             is_on = 0;
             total_time_on += difftime(time(NULL), last_turn_on_time);
           }
         char response[MAX_CMD_LEN];
         snprintf(response, sizeof(response), "%s: Bulb %d turned OFF", prefix, my_id);
-        send_to_controller(response);
+        send_response(msg.sender_id, response, is_manual_override);
       
-      } else if (strncmp(msg.command, "info", 4) == 0) {
+      } 
+      
+
+      else if (strncmp(msg.command, "info", 4) == 0) {
         // computes the startup time even if the bulb is currently ON
         long current_time_on = (long)total_time_on;
         if (is_on) {
@@ -111,10 +137,19 @@ int main(int argc, char *argv[]) {
                  "INFO: Bulb ID %d | Status: %s | Total time on: %ld sec", 
                  my_id, is_on ? "ON" : "OFF", current_time_on);
                  
-        send_to_controller(info_buffer);
+        send_response(msg.sender_id, info_buffer, is_manual_override);
       }
+
+
+      else if (strncmp(msg.command, "set_parent ", 11) == 0) {
+        // Updates the parent_id when recieving the link command
+        sscanf(msg.command, "set_parent %d", &parent_id);
+        printf("[Bulb %d] Parent updated to %d\n", my_id, parent_id);
+      }
+
+
       else {
-        send_to_controller("ERR: Unsupported command");
+        send_response(msg.sender_id, "ERR: Unsupported command", is_manual_override);
       }
     }
   }
